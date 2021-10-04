f61b330eb408b9c457e83e348eca6ba8f73aeb8b
Mubeen <mdmubeen1307@gmail.com>
2021-10-04 22:39:35 +0530

    Added keyboard events

--- snakeGame/script.js
+++ snakeGame/script.js
@@ -8,8 +8,8 @@ function initial(){
   canvas.width = W = 1000;
 
   // to draw on the canvas
-  const  pen = canvas.getContext('2d');
-  cellSize = 66;
+   pen = canvas.getContext('2d');
+   cellSize = 66;
   // create the snake as an object
 
   snake = {
@@ -34,19 +34,81 @@ function initial(){
        
       }
     },
+     
+    // snake knows how to update itself
+    updateSnake : function(){
+      // console.log('Updating snake');
+
+      // // extract a cell from last of array and add it to the front
+      // this.cells.pop();
+      // var headX = this.cells[0].x;
+      // var headY = this.cells[0].y;
+
+      // // move ahead by 1 in x co-ordinate
+      // var X = headX+1;
+      // var Y = headY;
+      // this.cells.unshift({x:X, y:Y});
+
+      // update snake according to the direction property
+
+      this.cells.pop();
+
+      var headX = this.cells[0].x;
+      var headY = this.cells[0].y;
+      var nextX , nextY;
+      if(this.direction == "right"){
+          nextX = headX+1 ;
+          nextY = headY;
+      }
+      else if(this.direction == "left"){
+        nextX = headX-1;
+        nextY = headY;
+      }
+      else if(this.direction == "down"){
+        nextX = headX;
+        nextY = headY+1;
+      }
+      else if(this.direction == "Up"){
+        nextX = headX;
+        nextY = headY-1;
+      }
+
+      this.cells.unshift({x:nextX , y:nextY});
+    }
 
   };
 
   // call the create snake function to fill the cells
   snake.createSnake();
 
-
+  // Add a event listener on document object
+  function keyPressed(event){
+    
+    if(event.key == "ArrowRight"){
+       // update direction of snake to right'
+       snake.direction = "right";
+    }
+    else if(event.key == "ArrowLeft"){
+      snake.direction = "left";
+    }
+    else if(event.key == "ArrowDown"){
+      snake.direction = "down";
+    }
+    else if(event.key == "ArrowUp"){
+      snake.direction = "Up"
+    }
+    console.log(snake.direction);
+  }
+  document.addEventListener('keydown',keyPressed);
 
 }
 
 function draw(){
  
   // draw function to draw the snake on canvas
+
+  // erase the old frame first
+  pen.clearRect(0,0,W,H);
   snake.drawSnake();
 }
 
@@ -54,7 +116,7 @@ function draw(){
 // function to update snake accordingly
 
 function update(){
-
+  snake.updateSnake();
 
 
 }
