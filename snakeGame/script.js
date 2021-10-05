974bdb294d508d789376e7a7fd9f587a9d8b0f4b
Mubeen <mdmubeen1307@gmail.com>
2021-10-05 10:51:39 +0530

    Snake game created

--- snakeGame/script.js
+++ snakeGame/script.js
@@ -1,17 +1,30 @@
 
+
 // initial function
 function initial(){
    
   // get the canvas element
   const canvas  = document.getElementById('my-canvas');
-  canvas.height = H = 600;
+  canvas.height = H =580;
   canvas.width = W = 1000;
-
+   
   // to draw on the canvas
    pen = canvas.getContext('2d');
-   cellSize = 66;
-  // create the snake as an object
 
+   cellSize = 66;
+   gameOver = false;
+   score = 5;
+
+   // create image object for food
+   foodImg = new Image();
+   foodImg.src = "Assets/apple.png";
+   
+   trophyImg = new Image();
+   trophyImg.src = "Assets/trophy.png";
+
+   food = getRandomFood();
+ 
+  // create the snake as an object
   snake = {
     init_len : 5 , 
     color:"blue",
@@ -50,11 +63,21 @@ function initial(){
       // this.cells.unshift({x:X, y:Y});
 
       // update snake according to the direction property
-
-      this.cells.pop();
-
+     
+      // if the snake has eaten the food increase the length of snake
+      //  and also generate new food
       var headX = this.cells[0].x;
       var headY = this.cells[0].y;
+
+      if(headX == food.x && headY == food.y){
+        console.log('food eaten by snake');
+        score++;
+        food = getRandomFood();
+      }
+      else{
+        this.cells.pop();
+      }   
+
       var nextX , nextY;
       if(this.direction == "right"){
           nextX = headX+1 ;
@@ -74,6 +97,14 @@ function initial(){
       }
 
       this.cells.unshift({x:nextX , y:nextY});
+
+      // prevent the snake from going outside the window
+      var lastX = Math.floor(W/cellSize);
+      var lastY = Math.floor(H/cellSize);
+
+      if(this.cells[0].y < 0 || this.cells[0].x < 0 || this.cells[0].y > lastY || this.cells[0].x > lastX){
+        gameOver = true;
+      }
     }
 
   };
@@ -104,25 +135,56 @@ function initial(){
 }
 
 function draw(){
- 
   // draw function to draw the snake on canvas
 
   // erase the old frame first
   pen.clearRect(0,0,W,H);
+
   snake.drawSnake();
+  pen.fillStyle = food.color;
+ // pen.fillRect(food.x*cellSize , food.y*cellSize , cellSize,cellSize);
+ pen.drawImage(foodImg,food.x*cellSize , food.y*cellSize , cellSize,cellSize)
+ 
+ // draw trophy
+ pen.drawImage(trophyImg , 18 , 20 , cellSize,cellSize);
+ 
+ // score variable
+ pen.font = "20px";
+ pen.fillStyle="blue";
+ pen.fillText(score,50,50);
 }
 
 
 // function to update snake accordingly
-
 function update(){
   snake.updateSnake();
-
-
 }
 
+// function to generate food at random positions
+function getRandomFood(){
+
+ var foodX = Math.floor(Math.random()*(W-cellSize)/cellSize);
+ var foodY = Math.floor(Math.random()*(H-cellSize)/cellSize);
+
+  // food object
+
+  var food = {
+    x:foodX,
+    y:foodY,
+    color:"red"
+  };
+
+  return food;
+}
+
+
 function gameLoop(){
- 
+
+  if(gameOver == true){
+    clearInterval(f);
+    alert("Game Over");
+    return;
+  }
   // call update and draw function repeatedly
   draw();
   update();
@@ -130,9 +192,8 @@ function gameLoop(){
 }
 
 // call the initial function
-
 initial();
 
 // implement the game loop
-var f = setInterval(gameLoop,500);
+var f = setInterval(gameLoop,300);
 
